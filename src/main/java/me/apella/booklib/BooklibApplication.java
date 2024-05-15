7d461e3fac325bfa5ca86378b30cb3ed296e525d
apella1 <johncollons5@gmail.com>
2024-05-15 08:50:03 +0300

    security: implement token authentication

--- src/main/java/me/apella/booklib/BooklibApplication.java
+++ src/main/java/me/apella/booklib/BooklibApplication.java
@@ -2,8 +2,10 @@ package me.apella.booklib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BooklibApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/me/apella/booklib/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package me.apella.booklib.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> getByName(String role);
+}
